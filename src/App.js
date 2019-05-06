22a197fd29f04855a94f25c39910ae91e5216db5
Saikh Ali <ssaifali@in.ibm.com>
2019-05-06 23:47:52 +0530

    added h2 tag

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             <h1>Hello!</h1>
+            <h2>Automated deployment!</h2>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
